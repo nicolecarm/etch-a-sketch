0d88f3bdbb37d5a1329f7ffb9df8adbafcf00ac5
nicolecarm <nickimaslan@gmail.com>
2018-05-07 12:06:42 -0700

    raw basics adjust

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// const wrapper = document.querySelector('#wrapper');
+//
+//
+// const container = document.querySelector('#grid');
+
+// var gridSquare = document.createElement('div');
+// gridSquare.classList.add('gridSquare');
+// gridSquare.setAttribute('style', 'border: black; background: pink; height: 400px; width: 400px;');
+// container.appendChild(gridSquare);
+
+
+// function createGrid(x) {
+//   var x = prompt();
+//
+//   for (var rows = 0; rows < x; rows++) {
+//
+//     for (var column = 0; column < x; column++){
+//
+//         var gridSquare = document.createElement('div');
+//         gridSquare.classList.add('gridSquare');
+//         gridSquare.setAttribute('style', 'border: 1px dashed black; background: pink; display: grid-template; grid-column: repeat(x, 1fr);');
+//         container.appendChild(gridSquare);
+//
+//       }
+//
+//     }
+//
+//   }
