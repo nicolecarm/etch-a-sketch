47d3dfbf1d0b03730f8b670a6088f188e68f8e16
nicolecarm <nickimaslan@gmail.com>
2018-05-10 16:40:59 -0700

    made jquery work..grid works

--- scripts.js
+++ scripts.js
@@ -1,28 +1,28 @@
-// const wrapper = document.querySelector('#wrapper');
-//
-//
-// const container = document.querySelector('#grid');
-
-// var gridSquare = document.createElement('div');
-// gridSquare.classList.add('gridSquare');
-// gridSquare.setAttribute('style', 'border: black; background: pink; height: 400px; width: 400px;');
-// container.appendChild(gridSquare);
+const wrapper = document.querySelector('#wrapper');
 
 
-// function createGrid(x) {
-//   var x = prompt();
+const container = document.querySelector('.gridContainer');
+
 //
-//   for (var rows = 0; rows < x; rows++) {
-//
-//     for (var column = 0; column < x; column++){
-//
-//         var gridSquare = document.createElement('div');
-//         gridSquare.classList.add('gridSquare');
-//         gridSquare.setAttribute('style', 'border: 1px dashed black; background: pink; display: grid-template; grid-column: repeat(x, 1fr);');
-//         container.appendChild(gridSquare);
-//
-//       }
-//
-//     }
-//
-//   }
+
+
+function createGrid(x) {
+  var x = prompt();
+
+  $('.gridContainer').css('grid-template-columns', 'repeat('+x+', 1fr)');
+
+  for (var rows = 0; rows < x; rows++) {
+
+    for (var column = 0; column < x; column++){
+
+        var gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquare');
+        // gridSquare.setAttribute('style', 'border: 1px dashed black; background: pink;');
+        container.appendChild(gridSquare);
+
+      }
+
+    }
+
+  }
+createGrid();
